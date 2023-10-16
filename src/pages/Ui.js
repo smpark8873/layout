57517b75552ad5907dfdafa3316e411601679448
smpark8873 <smpark@banaple.co.kr>
2023-10-16 15:29:59 +0900

    조건부렌더링

--- src/pages/Ui.js
+++ src/pages/Ui.js
@@ -1,4 +1,172 @@
 
+
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',  
+}, {
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
+
+export function getImageUrl(person) {
+  return (
+    'https://i.imgur.com/' +
+    person.imageId +
+    's.jpg'
+  );
+}
+
+
+export default function List() {
+  const chemists = people.filter(person =>
+    person.profession === 'chemist'
+  );
+  const listItems = chemists.map(person =>
+    <li>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return <ul style={{padding: '150px 50px 50px'}}>{listItems}</ul>;
+}
+
+
+
+// 챌린지 3/3 : _ _일련의 ? :to if및 변수 리팩터링
+// 이 Drink구성 요소는 일련의 조건을 사용하여 소품이 인지 인지 ? :에 따라 다른 정보를 표시합니다 . 
+// 문제는 각 음료에 대한 정보가 여러 조건에 걸쳐 분산되어 있다는 것입니다. 
+// 세 가지 조건 대신 단일 문을 사용하도록 이 코드를 리팩터링합니다 .name"tea""coffee"if? :
+
+// function Drink({ name }) {
+//   let part, caffeine, age;
+//   if (name === 'tea') {
+//     part = 'leaf';
+//     caffeine = '15–70 mg/cup';
+//     age = '4,000+ years';
+//   } else if (name === 'coffee') {
+//     part = 'bean';
+//     caffeine = '80–185 mg/cup';
+//     age = '1,000+ years';
+//   }
+//   return (
+//     <section className="wrap">
+//       <h1>{name}</h1>
+//       <dl>
+//         <dt>Part of plant</dt>
+//         <dd>{part}</dd>
+//         <dt>Caffeine content</dt>
+//         <dd>{caffeine}</dd>
+//         <dt>Age</dt>
+//         <dd>{age}</dd>
+//       </dl>
+//     </section>
+//   );
+// }
+
+// export default function DrinkList() {
+//   return (
+//     <div>
+//       <Drink name="tea" />
+//       <Drink name="coffee" />
+//     </div>
+//   );
+// }
+
+
+
+
+// 챌린지 2/3 : _ _다음으로 항목 중요도 표시&& 
+// 이 예에서 각각은 Item숫자 importanceprop을 받습니다. &&" (중요도: X) "를 기울임꼴로 렌더링하려면 연산자를 사용하세요 . 단, 중요도가 0이 아닌 항목에만 해당됩니다. 항목 목록은 다음과 같이 표시됩니다.
+// 우주복 (중요도: 9)
+// 황금잎이 달린 헬멧
+// 탐의 사진 (중요도: 6)
+// 두 라벨 사이에 공백을 추가하는 것을 잊지 마세요!
+
+// function Item({ name, importance }) {
+//   return (
+//     <li className="item">
+//       {name} 
+//       {importance > 0 && ' '}
+//       {importance > 0 && <i>(Importance: {importance})</i>}
+//     </li>
+//   );
+// }
+
+// export default function PackingList() {
+//   return (
+//     <section className="wrap">
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item 
+//           importance={9} 
+//           name="Space suit" 
+//         />
+//         <Item 
+//           importance={0} 
+//           name="Helmet with a golden leaf" 
+//         />
+//         <Item 
+//           importance={6} 
+//           name="Photo of Tam" 
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
+
+
+
+// 챌린지 1/3 : _ _완료되지 않은 항목에 대한 아이콘 표시? : 
+// 조건 연산자( cond ? a : b)를 사용하여 ❌ if isPackedis not 을 렌더링합니다 true.
+// function Item({ name, isPacked }) {
+//   return (
+//     <li className="item">
+//       {name} {isPacked ? '✔' : '❌'}
+//     </li>
+//   );
+// }
+
+// export default function PackingList() {
+//   return (
+//     <section className="wrap">
+//       <h1>Sally Ride's Packing List</h1>
+//       <ul>
+//         <Item 
+//           isPacked={true} 
+//           name="Space suit" 
+//         />
+//         <Item 
+//           isPacked={true} 
+//           name="Helmet with a golden leaf" 
+//         />
+//         <Item 
+//           isPacked={false} 
+//           name="Photo of Tam" 
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
+
 // 챌린지 3/3 : _ _children소품 에 JSX 전달하기
 // Card아래 마크업에서 구성 요소를 추출 하고 childrenprop을 사용하여 다른 JSX를 전달합니다.
 
@@ -11,8 +179,6 @@
 //     </div>
 //   );
 // }
-
-
 
 // export default function Profile() {
 //   return (
@@ -110,38 +276,6 @@
 //         </section>
 //     );
 // }
-
-{/*
-const baseUrl = 'https://i.imgur.com/';
-const person = {
-  name: 'Gregorio Y. Zara',
-  imageId: '7vQD0fP',
-  imageSize: 's',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
-
-
-export default function TodoList() {
-  return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos https://i.imgur.com/7vQD0fPs.jpg</h1>
-      <img
-        className="avatar"
-        src={baseUrl + person.imageId + person.imageSize + '.jpg'}
-        alt={person.name}
-      />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-    </div>
-  );
-}
-*/}
 
 
 // function Avatar({ person,size }) {
