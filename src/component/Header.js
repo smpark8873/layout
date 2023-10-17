6862c40841b482e6b8cc11363d55966689682c0d
smpark8873 <smpark@banaple.co.kr>
2023-10-17 14:01:52 +0900

    eventHandle

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -10,8 +10,7 @@ const HeaderWrap = styled.div`
     width: 100%;
     min-height: 100px;
     background-color: #333; 
-    color: #fff;    
-    padding: 30px;
+    color: #fff;  
     box-sizing: border-box;
     z-index: 100;
     h1 {
@@ -35,30 +34,69 @@ const Nav = styled.div`
         flex-wrap: nowrap;
         list-style: none;
         margin: 0; 
+        height: 100px;
         li { 
-            a{
+            &:hover .depth2 {
+                height: auto;               
+            }
+            position: relative;
+            > a {
                 display: block;
                 padding: 0 40px;
                 color: #fff;
-                line-height: 40px; 
+                line-height: 100px; 
                 font-size: 22px;
                 text-decoration: 0;               
                 &.active {
                     font-weight: bold;
                     color: pink;
+                }                
+            }
+            .depth2 {
+                position: absolute;
+                top: 80px;
+                left: 0;
+                width: 100%;
+                height: 0; 
+                overflow: hidden;
+                background-color: #fff;
+                border-radius: 10px;
+                box-shadow: 3px 3px 3px 3px rgba(0,0,0,0.05);
+                transition: height 0.5s ease-in-out;
+                transform-origin: top;
+
+                a {
+                    display: block;
+                    padding: 0 40px;
+                    color: #333;
+                    font-size: 18px;
+                    line-height: 40px;
+                    
+                    &:hover {
+                        font-weight: bold;
+                    }
                 }
+
             }
         }
     }
 `;
 
-function Header(props) {
-    return (
+
+function Header(props) {    
+    return (       
         <HeaderWrap>
             <h1><NavLink to="/">LOGO</NavLink></h1>            
             <Nav>
                 <ul>
-                    <li><NavLink to="/Intro">ReactPlayer</NavLink></li>
+                    <li>
+                        <NavLink to="/Intro">ReactPlayer</NavLink>
+                        <div className='depth2'>
+                            <NavLink to="/Intro">depth2_1_1</NavLink>
+                            <NavLink to="/Intro">depth2_1_2</NavLink>
+                            <NavLink to="/Intro">depth2_1_3</NavLink>
+                        </div>
+                    </li>
                     <li><NavLink to="/Business">Animation1</NavLink></li>
                     <li><NavLink to="/Animation">Animation2</NavLink></li>
                     <li><NavLink to="/ScrollAnimation">ScrollAnimation</NavLink></li>
--- src/pages/Contents.js
+++ src/pages/Contents.js
@@ -1,38 +1,102 @@
-import React, { useState } from 'react';
+import Reac from 'react';
+//import React, { useState } from 'react';
+
 import styled from "styled-components";
 
-const ContentWrap = styled.div`    
-    min-height: 1000px;
-    .btn {
-        width: 100px;
-        height: 40px;
-        font-size: 30px; 
-        line-height: 38px;
-        border: 1px solid #ddd;
-        border-radius: 5px;
+// const ContentWrap = styled.div`    
+//     min-height: 1000px;
+//     .btn {
+//         width: 100px;
+//         height: 40px;
+//         font-size: 30px; 
+//         line-height: 38px;
+//         border: 1px solid #ddd;
+//         border-radius: 5px;
+//     }
+// `;
+
+const ActiveItem = styled.div`
+    min-height: 1000px; 
+    margin-top: 100px;
+    li {
+        line-height: 50px;
+        font-size: 20px;    
+        &.active {
+            font-weight: bold;
+            color: blue;
+        }    
     }
-`
+
+`;
 
 const Contents = () => {
-    const [number, setNumber] = useState(0); 
+    //const [isActive, setIsActive] = useState(false);
 
-    const onIncrease = () => {
-        // setNumber 함수를 통한 state 값 변경
-        setNumber(prevNumber => prevNumber + 1);
-      }
-    
-      const onDecrease = () => {
-        // setNumber 함수를 통한 state 값 변경
-        setNumber(prevNumber => prevNumber - 1);
-      }
-    
+    const handleClick = event => {   
+        event.currentTarget.classList.toggle('active').siblings.classList.remove('active');         
+       // setIsActive(current => !current);
+    };  
+
     return (
-        <ContentWrap>
-            <h1>{number}</h1>
-            <button className="btn" onClick={onIncrease}>+</button>
-            <button className="btn" onClick={onDecrease}>-</button>
-        </ContentWrap>
+        <ActiveItem>
+            <ul>
+                <li onClick={handleClick}>
+                    1. nav
+                </li>
+                <li onClick={handleClick}>
+                    2. swiper
+                </li>
+                <li onClick={handleClick}>
+                    3. video
+                </li>
+                <li onClick={handleClick}>
+                    4. animation
+                </li>
+            </ul>
+
+            {/* <ul>
+                <li className={isActive ? 'active' : ''} onClick={handleClick}>
+                    1. nav
+                </li>
+                <li className={isActive ? 'active' : ''} onClick={handleClick}>
+                    2. swiper
+                </li>
+                <li className={isActive ? 'active' : ''} onClick={handleClick}>
+                    3. video
+                </li>
+                <li className={isActive ? 'active' : ''} onClick={handleClick}>
+                    4. animation
+                </li>
+            </ul> */}
+        </ActiveItem>
     );
 }
 
 export default Contents;
+
+
+
+// 클릭시 카운트 증감
+// const Contents = () => {
+//     const [number, setNumber] = useState(0); 
+
+//     const onIncrease = () => {
+//         // setNumber 함수를 통한 state 값 변경
+//         setNumber(prevNumber => prevNumber + 1);
+//     }
+
+//     const onDecrease = () => {
+//     // setNumber 함수를 통한 state 값 변경
+//     setNumber(prevNumber => prevNumber - 1);
+//     }  
+
+//     return (
+//         <ContentWrap>
+//             <h1>{number}</h1>
+//             <button className="btn" onClick={onIncrease}>+</button>
+//             <button className="btn" onClick={onDecrease}>-</button>
+//         </ContentWrap>
+//     );
+// }
+
+// export default Contents;
