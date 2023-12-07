986cbcf7f01e3bcf0a6c25fd5a03e20571fd56f4
smpark8873 <smpark@banaple.co.kr>
2023-12-07 16:26:37 +0900

    state

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -192,28 +192,83 @@ import { useState } from 'react';
 // 사용자가 구매 버튼을 누를 때마다 보류카운터가 1 씩 증가해야 합니다. 
 // 3초후에는 보류중 카운터가 감소하고 완료 카운터가 증가해야 합니다.
 
-export default function Test2() {
-  const[pending, setPending] = useState(0);
-  const[completed, setCompleted] = useState(0);
+// export default function Test2() {
+//   const[pending, setPending] = useState(0);
+//   const[completed, setCompleted] = useState(0);
 
-  async function handleClick() {
-    setPending(p => p+1);
-    await delay(3000);
-    setPending(pending -1);
-    setCompleted(completed+1);
+//   async function handleClick() {
+//     setPending(p => p+1);
+//     await delay(3000);
+//     setPending(pending -1);
+//     setCompleted(completed+1);
+//   }
+
+//   return (
+//     <div className='wrap'>
+//       <h3>Pending  :{pending}</h3>
+//       <h3>completed : {completed}</h3>
+//       <button onClick={handleClick}>Buy</button>
+//     </div>
+//   );
+// }
+
+// function delay(ms) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve,ms);
+//   });
+// }
+
+export default function Test() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  if(status ==='success') {
+    return <h1>That's right!</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+
+    try {
+      await submitForm(answer);
+      setStatus('success');     
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
   }
 
   return (
     <div className='wrap'>
-      <h3>Pending  :{pending}</h3>
-      <h3>completed : {completed}</h3>
-      <button onClick={handleClick}>Buy</button>
+      <h2>City quize</h2>
+      <p>In which city is there a billboard that turns air into drinkable water?</p>
+      <form onSubmit={handleSubmit}>
+        <textarea value={answer} onChange={handleTextareaChange} disabled={status === 'submitting'} />
+        <br />
+        <button disabled={answer.length === 0 || status === 'submitting'}>Submit</button>
+        {error !== null && 
+        <p className="Error">{error.message}</p>}
+      </form>
     </div>
-  );
+  )
 }
 
-function delay(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve,ms);
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
   });
 }
